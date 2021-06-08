28998229cc576cc2d32314853d79f177367d35d1
tafriheman <smsmt76768@gmail.com>
2021-06-08 10:47:43 +0430

    remote server debug

--- src/config/app.js
+++ src/config/app.js
@@ -1,6 +1,6 @@
 export default {
   BASE_SOCKET_URL: 'http://zapptaxi.com:3005',
-  BASE_URL: 'http://zapptaxi:3005/api/',
+  BASE_URL: 'http://zapptaxi.com:3005/api/',
   ICON_IMAGE_URL: 'http://zapptaxi.com:3002/images/icons/',
   DRIVER_IMAGE_URL: 'http://zapptaxi.com:3002/images/driver/',
   PASSENGER_IMAGE_URL: 'http://zapptaxi.com:3002/images/passenger/'
